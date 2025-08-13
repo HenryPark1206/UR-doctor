719ce56ea42e3c7673e52d4190de76780e421e70
HenryPark1206 <161937092+HenryPark1206@users.noreply.github.com>
2025-08-13 16:09:29 +0900

    Add files via upload

---
+++ data_management.py
@@ -0,0 +1,35 @@
+import json
+def get_path(file_name):
+    if file_name == "illnesses": return "data/illnesses.json"
+    if file_name == "symptoms": return "data/symptoms.json"
+    if file_name == "data_management": return "data_management.py"
+    return None
+def read(file_path):
+    with open(f'{file_path}', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+def name_format(name):
+    parsed = str()
+    for c in name:
+        if c.isalpha():parsed += c
+        else: parsed += ' '
+    return parsed.lower()
+def add_illness(name: str, symptoms: list, solution: str):
+    path = get_path("illnesses")
+    data = read(path)
+    data[name] = dict()
+    data[name]["symptoms"] = symptoms
+    data[name]["solution"] = solution
+    with open(f'{path}', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+        print(f"illness added--> NAME {name} SYMPTOMS {symptoms} SOLUTION {solution}")
+
+def add_symptoms(symptoms: list):
+    path = get_path("symptoms")
+    data = read(path)
+    data["symptoms"] += symptoms
+    with open(f'{path}', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+        print(f"symptoms added--> {symptoms}")
+add_illness("common cold", ["sore throat", "runny nose", "sneeze", "cough"], "drink water")
+add_symptoms(["sore throat"])
---
+++ source.py
@@ -0,0 +1,4 @@
+"""
+https://www.nhsinform.scot/illnesses-and-conditions/a-to-z/
+https://dph.illinois.gov/topics-services/diseases-and-conditions/diseases-a-z-list.html
+"""
